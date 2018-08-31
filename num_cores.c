f52def2ca30e464d602ed8683aa46935515b6228
sam <samuelzheng11@gmail.com>
2018-08-31 15:31:06 +1200

    Print message with number of cores

--- num_cores.c
+++ num_cores.c
@@ -4,13 +4,8 @@
 #include <unistd.h>
 #include <sys/sysinfo.h>
 
-int getNumberOfCores(){
-    return get_nprocs_conf();
-}
-
 int main(int argc, char** argv) {
     // create a concurrent dispatch queue
-    int item = getNumberOfCores();
-    printf("%i\n", item);
+    printf("This program has access to %i number of cores\n", get_nprocs_conf());
     return 0;
 }
