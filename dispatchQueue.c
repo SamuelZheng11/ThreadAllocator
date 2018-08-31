9a9ea8b351a8be2c2629395bc158f3fca8497f75
sam <samuelzheng11@gmail.com>
2018-08-31 15:29:30 +1200

    Print out number of cores

--- dispatchQueue.c
+++ dispatchQueue.c
@@ -1,9 +1,14 @@
-#include "dispatchQueue.h";
+#include "dispatchQueue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-
 dispatch_queue_t *dispatch_queue_create(queue_type_t queueType) {
+    dispatch_queue_t *queue;
+    return dispatch_queue_create(CONCURRENT);
+}
 
+void dispatch_queue_destroy(dispatch_queue_t *queue) {
+    dispatch_queue_t *queue;
+    dispatch_queue_destroy(queue);
 }
--- num_cores.c
+++ num_cores.c
@@ -1,4 +1,16 @@
-#include "dispatchQueue.h";
+#include "dispatchQueue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
+
+int getNumberOfCores(){
+    return get_nprocs_conf();
+}
+
+int main(int argc, char** argv) {
+    // create a concurrent dispatch queue
+    int item = getNumberOfCores();
+    printf("%i\n", item);
+    return 0;
+}
